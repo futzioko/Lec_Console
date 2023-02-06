d6ba65348c07d94a4a27ed3e75b7772cab6a52a5
qrezko <sabirova.04@bk.ru>
2023-02-06 20:53:39 +0300

    Added Cat's class

--- Lec_Console/Animal.cs
+++ Lec_Console/Animal.cs
@@ -6,14 +6,28 @@
         
         public void Eat()
         {
-            System.Console.WriteLine("Животное кушает");
+            System.Console.WriteLine("\tЖивотное кушает");
         }
 
         public void Move()
         {
-            System.Console.WriteLine("Животное перемещается");
+            System.Console.WriteLine("\tЖивотное перемещается");
         }
 
-
     }
+
+    class Cat: Animal
+    {
+        public Cat(string name) { this.name = name; }
+        public void Eat()
+        {
+            System.Console.WriteLine($"\tКошка {name} кушает Whiskas 15 лет и не жалуется");
+        }
+
+        public void Move()
+        {
+            System.Console.WriteLine("\tКошка перемещается на 4-х лапах");
+        }
+    }
+
 }
--- Lec_Console/Program.cs
+++ Lec_Console/Program.cs
@@ -10,6 +10,18 @@ namespace Lec_Console
     {
         static void Main(string[] args)
         {
+            Animal animal = new Animal();
+
+            animal.Eat();
+            animal.Move();
+            
+            Console.WriteLine("\n");
+
+            Cat cat = new Cat("Муся");
+            cat.Eat();
+            cat.Move();
+
+            Console.ReadKey();
         }
     }
 }
