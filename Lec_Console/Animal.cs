065086d6bbf6df503b9da2f32998bfc6de4b9131
qrezko <sabirova.04@bk.ru>
2023-02-06 20:44:05 +0300

    Добавьте файлы проекта.

---
+++ Lec_Console/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace Lec_Console
+{
+    class Animal
+    {
+        public string name;
+        
+        public void Eat()
+        {
+            System.Console.WriteLine("Животное кушает");
+        }
+
+        public void Move()
+        {
+            System.Console.WriteLine("Животное перемещается");
+        }
+
+
+    }
+}
